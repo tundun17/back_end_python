880a0982df66c93adb680e89dfc3eb2c39881c2c
tundun17 <dh6448855@gmail.com>
2026-05-20 13:07:45 +0700

    update first test

--- smarthome/views.py
+++ smarthome/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Xin chào. Bạn đã đến trang Smart Home Backend")
